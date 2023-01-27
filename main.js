d5856094a3b75046473ee755a6665b308b490908
Ale-GV <67850193+Ale-GV@users.noreply.github.com>
2023-01-27 08:58:28 -0600

    Uploading my existing files (html, css, js)

---
+++ main.js
@@ -0,0 +1,30 @@
+
+// Set the date we're counting down to
+var countDownDate = new Date().setHours(12,0,0);
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("demo").innerHTML = "Order within " + hours + "h "
+  + minutes + "m " + seconds + "s " + "to qualify for rush shipping.";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("demo").innerHTML = "You do not qualify for free shipping.";
+  }
+}, 1000);
+
